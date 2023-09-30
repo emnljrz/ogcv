c5f2fa47258f6b75ea877056d7b8e3a9ed5718eb
emnljrz+git <99168051+emjrz@users.noreply.github.com>
2023-10-01 02:51:02 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'resume';
+export class AppComponent implements OnInit {
+	public title = 'resume';
+	public cursorShow = false;
+
+	constructor() { }
+
+	ngOnInit(): void {
+		
+	}
 }
